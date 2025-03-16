a1911a6e00072644c54c32faaca0d9b811a33b86
Tamiris Lemes <tamiris.lemes18@gmail.com>
2025-03-16 19:54:01 -0300

    Salvar dados em csv do twitter

--- x_data.py
+++ x_data.py
@@ -25,28 +25,29 @@ driver.maximize_window()
 driver.execute_script("document.body.style.zoom='70%'")
 time.sleep(10)
 
-# Cria um DataFrame vazio (ou carrega um existente)
+
 df_tweets = pd.DataFrame(columns=["Tipo", "Texto"])
 
-# find_tweet(driver, count)
-# df_tweets = extract_tweets(driver, df_tweets)
-# print_dataframe(df_tweets)
-# # Voltar
-# click_element(driver, locator="/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/button", locator_type=By.XPATH)
-
-max_iteracoes = 5  # Número máximo de iterações
-count = 0  # Contador para iterar sobre os tweets
+max_iteracoes = 5 
+count = 0  
 
 while count < max_iteracoes:
     print(f"Processando tweet {count + 1} de {max_iteracoes}")
 
-    if find_tweet(driver, count):
-        df_tweets = extract_tweets(driver, df_tweets)
-        print_dataframe(df_tweets)
-        # Volta para a timeline
-        click_element(driver, locator='//*[@aria-label="Back"]', locator_type=By.XPATH)
-        time.sleep(3)
-        count += 1
-    else:
-        print("Nenhum tweet encontrado. Interrompendo o loop.")
-        break
+    find_tweet(driver, count)
+    df_tweets = extract_tweet(driver, df_tweets)
+
+    print_dataframe(df_tweets)
+
+    # Volta para a timeline (clica no botão "Back")
+    click_element(
+        driver,
+        locator='//*[@aria-label="Back"]',  # XPath para o botão "Back"
+        locator_type=By.XPATH
+    )
+
+    # Aguarda o carregamento da timeline
+    time.sleep(3)
+
+    # Incrementa o contador para processar o próximo tweet
+    count += 1
