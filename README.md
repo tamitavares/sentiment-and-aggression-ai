32e4726e9aa8dfc19aa94734c2b9189de8017ee8
Tamiris Lemes <tamiris.lemes18@gmail.com>
2025-03-16 14:23:26 -0300

    Twitter (X) login

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# sentiment-and-aggression-ai
+🧠 Sentiment and Aggression AI
+This project focuses on analyzing sentiment and aggression in text using AI models.
+
+🚀 Getting Started
+1️⃣ Clone the Repository
+    git clone https://github.com/your-username/sentiment-and-aggression-ai.git
+    cd sentiment-and-aggression-ai
+2️⃣ Set Up a Virtual Environment
+    python -m venv .venv
+    .venv\Scripts\activate
+3️⃣ Install Dependencies
+    pip install -r requirements.txt
+
+
+
--- x_data.py
+++ x_data.py
@@ -3,3 +3,14 @@ from config import *
 from functions import *
 
 open_page(x_page_futebol)
+# click_element(driver, locator="/html/body/div/div[1]/div/div[1]/div[2]/svg", locator_type=By.XPATH)
+# # click_element(driver, locator="/html/body/div/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[4]/a/div", locator_type=By.XPATH)
+
+# Email
+write_data(driver, locator="/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[4]/label/div/div[2]/div/input", data=username, locator_type=By.XPATH)
+# Avançar 
+click_element(driver, locator="/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/button[2]", locator_type=By.XPATH)
+# Password
+write_data(driver, locator="/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input", data=password, locator_type=By.XPATH)
+# Entrar
+click_element(driver, locator="/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/button", locator_type=By.XPATH)
